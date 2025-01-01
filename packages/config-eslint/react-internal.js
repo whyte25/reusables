1889828315c84cb24e5b735c4d4c768c0db7ec44
whyte25 <fasifeoluwa3@gmail.com>
2025-01-01 11:56:50 +0100

    chore: add shared ESLint configurations for Next.js and React; remove legacy configurations

--- packages/config-eslint/react-internal.js
+++ packages/config-eslint/react-internal.js
@@ -1,5 +1,4 @@
 import js from "@eslint/js";
-import pluginNext from "@next/eslint-plugin-next";
 import eslintConfigPrettier from "eslint-config-prettier";
 import pluginReact from "eslint-plugin-react";
 import pluginReactHooks from "eslint-plugin-react-hooks";
@@ -8,31 +7,22 @@ import tseslint from "typescript-eslint";
 import { config as baseConfig } from "./base.js";
 
 /**
- * A custom ESLint configuration for libraries that use Next.js.
+ * A custom ESLint configuration for libraries that use React.
  *
- * @type {import("eslint").Linter.Config}
- * */
-export const nextJsConfig = [
+ * @type {import("eslint").Linter.Config} */
+export const config = [
   ...baseConfig,
   js.configs.recommended,
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
   {
-    ...pluginReact.configs.flat.recommended,
     languageOptions: {
       ...pluginReact.configs.flat.recommended.languageOptions,
       globals: {
         ...globals.serviceworker,
+        ...globals.browser,
       },
-    },
-  },
-  {
-    plugins: {
-      "@next/next": pluginNext,
-    },
-    rules: {
-      ...pluginNext.configs.recommended.rules,
-      ...pluginNext.configs["core-web-vitals"].rules,
     },
   },
   {
