f91bb02a20c2ca8fa629f1b6ac86d0a59f5af96f
whyte25 <fasifeoluwa3@gmail.com>
2025-01-07 12:16:46 +0100

    feat:  update build scripts and add components configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,29 @@
-import { withContentCollections } from '@content-collections/next';
+import { withContentCollections } from "@content-collections/next";
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/r/index",
+        destination: "/r/index.json",
+        permanent: true,
+      },
+      {
+        source: "/r/:name((?!index\\.json|styles/).*)",
+        destination: "/r/styles/default/:name.json",
+        permanent: true,
+        missing: [
+          {
+            type: "query",
+            key: "_redirected",
+            value: undefined,
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withContentCollections(config);
