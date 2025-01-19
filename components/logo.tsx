c88899b40233a92930f65d1ad485610c077ca008
whyte25 <fasifeoluwa3@gmail.com>
2025-01-19 12:39:40 +0100

    feat: implement favicon and logo compponent

---
+++ components/logo.tsx
@@ -0,0 +1,10 @@
+import { RefreshCcwDot } from "lucide-react";
+
+export const Logo = () => {
+  return (
+    <div className="flex items-center gap-2">
+      <RefreshCcwDot size={17} />
+      <p>Reusables</p>
+    </div>
+  );
+};
