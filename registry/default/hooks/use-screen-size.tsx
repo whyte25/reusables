73324f226d58f8467bd0da351283a48e8f9d645f
whyte25 <fasifeoluwa3@gmail.com>
2025-02-16 21:27:19 +0100

    feat: update screensizes functionality and other minior changes

--- registry/default/hooks/use-screen-size.tsx
+++ registry/default/hooks/use-screen-size.tsx
@@ -13,34 +13,48 @@ const useScreenSize = (): ScreenSizeResult => {
   const [screenSize, setScreenSize] = useState<ScreenSize>(undefined)
   const [isMobile, setIsMobile] = useState(false)
 
-  const handleResize = () => {
-    const width = window.innerWidth
-    if (width <= 425) {
-      setScreenSize("xs")
-      setIsMobile(true)
-    } else if (width <= 600) {
-      setScreenSize("sm")
-      setIsMobile(true)
-    } else if (width <= 758) {
-      setScreenSize("md")
-      setIsMobile(false)
-    } else if (width <= 1200) {
-      setScreenSize("lg")
-      setIsMobile(false)
-    } else if (width <= 1400) {
-      setScreenSize("2xl")
-      setIsMobile(false)
-    } else if (width >= 1800) {
-      setScreenSize("4k")
-      setIsMobile(false)
-    }
-  }
-
   useEffect(() => {
-    handleResize()
-    window.addEventListener("resize", handleResize)
+    const mediaQueries = {
+      xs: window.matchMedia("(max-width: 425px)"),
+      sm: window.matchMedia("(min-width: 426px) and (max-width: 600px)"),
+      md: window.matchMedia("(min-width: 601px) and (max-width: 768px)"),
+      lg: window.matchMedia("(min-width: 769px) and (max-width: 1200px)"),
+      _2xl: window.matchMedia("(min-width: 1201px) and (max-width: 1400px)"),
+      _4k: window.matchMedia("(min-width: 1800px)"),
+    }
+
+    const updateScreenSize = () => {
+      if (mediaQueries.xs.matches) {
+        setScreenSize("xs")
+        setIsMobile(true)
+      } else if (mediaQueries.sm.matches) {
+        setScreenSize("sm")
+        setIsMobile(true)
+      } else if (mediaQueries.md.matches) {
+        setScreenSize("md")
+        setIsMobile(false)
+      } else if (mediaQueries.lg.matches) {
+        setScreenSize("lg")
+        setIsMobile(false)
+      } else if (mediaQueries._2xl.matches) {
+        setScreenSize("2xl")
+        setIsMobile(false)
+      } else if (mediaQueries._4k.matches) {
+        setScreenSize("4k")
+        setIsMobile(false)
+      }
+    }
+
+    updateScreenSize()
+
+    Object.values(mediaQueries).forEach((query) => {
+      query.addEventListener("change", updateScreenSize)
+    })
+
     return () => {
-      window.removeEventListener("resize", handleResize)
+      Object.values(mediaQueries).forEach((query) => {
+        query.removeEventListener("change", updateScreenSize)
+      })
     }
   }, [])
 
